823b23548324a4c29b8dd525c17808fdca491627
Ohazulume C. Fabian <fabianohaz@gmail.com>
2023-10-12 09:23:48 +0100

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Flask App that integrates with AirBnB static HTML Template
-"""
+""" Script that imports a Blueprint and runs Flask """
 from api.v1.views import app_views
 from flask import Flask, jsonify, make_response, render_template, url_for
 from flask_cors import CORS, cross_origin
